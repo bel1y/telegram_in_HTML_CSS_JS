189b934c072773d04811956d7d78a7e8139eb6a9
bel1y <132271045+bel1y@users.noreply.github.com>
2023-09-25 15:56:28 +0300

    dfg
    dfgh

--- telegram/main.js
+++ telegram/main.js
@@ -4,3 +4,21 @@ function openMenu() {
 function closeMenu() {
     document.querySelector(".setting").style="left:-100%"
 }
+
+function openMenu2() {
+    document.querySelector(".setting2").style="left:0%"
+}
+function closeMenu2() {
+    document.querySelector(".setting2").style="left:-100%"
+}
+function openChnge() {
+    document.querySelector(".change_bgc").style="left: 0%"
+}
+
+function backMenu() {
+    document.querySelector(".change_bgc").style="left: -1000%"
+}
+
+function bgc_change() {
+    document.querySelector(".text_place").style=""
+}
